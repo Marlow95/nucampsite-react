d98ebba720fb088addbdebe4633e29e195369086
Marlow Collins <marlowcollins95@gmail.com>
2021-09-11 12:32:15 -0500

    campsite info update

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { LocalForm, Control, Errors } from 'react-redux-form';
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({campsite}){
     return (<div className="col-md-5 m-1">
@@ -14,6 +19,81 @@ function RenderCampsite({campsite}){
     </div>)
 }
 
+class CommentForm extends React.Component{
+    constructor(props) {
+        super(props);
+        this.togglebtn = this.togglebtn.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+          isModalOpen: false,
+          touched: {
+            rating: false,
+            author: false,
+            comment: false,
+            }
+        };
+    }
+
+    togglebtn() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        console.log('Current state is: ' + JSON.stringify(values));
+        alert('Current state is: ' + JSON.stringify(values));
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                <Button outline color="secondary" onClick={this.togglebtn}>
+                    <i className="fa fa-pencil fa-lg" />Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.togglebtn}>
+                <ModalHeader toggle={this.togglebtn}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)} rows={6}>
+                        <div className="form-group" >
+                        <Label htmlFor="rating">Rating</Label>
+                        <Control.select model=".rating" id="rating" name="rating"  value="1" className="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Control.select>
+                        </div>
+                        <div className="form-group" >
+                        <Label htmlFor="author">Your Name</Label>
+                        <Control.text  model=".author" id="author" name="author"  placeholder="Your Name" className="form-control" 
+                        validators={{
+                            required, 
+                            minLength: minLength(2),
+                            maxLength: maxLength(15)
+                        }}/>
+                        <Errors className="text-danger" model=".author"show="touched" component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                        </div>
+                        <div className="form-group" >
+                        <Label htmlFor="text">Comments</Label>
+                        <Control.textarea  model=".text" id="comment" name="comment" className="form-control"/>
+                        </div>
+                        <Button color="success">Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        )
+    }
+}
+
 function RenderComments({comments}){
     if(comments){
         return(
@@ -21,6 +101,7 @@ function RenderComments({comments}){
                 <h4>Comments</h4>
                 {comments.map(comment => <div key={comment.id}><p>{comment.text}</p><p>--{comment.author} , 
                 {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)}
+                <CommentForm />
             </div>
         )
     }
