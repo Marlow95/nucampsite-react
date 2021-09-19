d481ff22ad0879bb4446da2ee9f2d041f47a004f
Marlow Collins <marlowcollins95@gmail.com>
2021-09-18 21:50:18 -0500

    task 1 final

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderPartner({ partner }) {
     if (partner) {
         return (
             <React.Fragment>
-                <Media object className="w-150" src={partner.image} alt={partner.name} />
+                <Media object className="w-150" src={baseUrl + partner.image} alt={partner.name} />
                 <Media body className="ml-5 mb-4">
                     <Media heading>{partner.name}</Media>
                     {partner.description}
@@ -19,17 +21,54 @@ function RenderPartner({ partner }) {
         )
     }
 }
+ 
+function PartnerList(props){
 
-
-function About(props) {
-
-    const partners = props.partners.map(partner => {
+    const partners = props.partners.partners.map(partner => {
         return (
             <Media key={partner.id} tag="li">
                 <RenderPartner partner={partner} />
             </Media>
         );
     });
+    
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
+    return (
+        <div className="col mt-4">
+            <Media list>
+                {partners}
+            </Media>
+        </div>
+
+    )
+
+
+}
+
+function About(props) {
+
+
 
     return (
         <div className="container">
@@ -83,11 +122,7 @@ function About(props) {
                 <div className="col-12">
                     <h3>Community Partners</h3>
                 </div>
-                <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
-                </div>
+                <PartnerList partners={props.partners} />
             </div>
         </div>
     );
